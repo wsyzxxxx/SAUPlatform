13b0de9a36730f4b258376043fe8633e234d8562
PurslaneQAQ <1059109666@qq.com>
2017-06-03 11:42:39 +0800

    6.13_11:42

--- goods.php
+++ goods.php
@@ -50,6 +50,7 @@
 
 <div align = "center" style="margin-top: 1rem" >
 
+<form action = "goods.php" method = "post">
 <div class="input-group" style = "padding: 0.5rem; font-size: 1.2rem;">
 <input type="search" class="form-control" value="借用起止日期" readonly="readonly" style = "background-color: whiteSmoke; border-style: outset; text-align: center; width:29rem;height: 3rem;font-size: 1.3rem"/>
 <input type="date" class="form-control" placeholder="借出日期" style = "text-align: center; width: 30rem; height: 3rem;"/>
@@ -57,34 +58,35 @@
 </div>
 
 <div class="input-group" style = "padding: 0.5rem; margin-bottom: 0.4rem">
-<select class="form-control" name = "condition" style = "background-color: whiteSmoke;border-style: outset; padding-left: 11rem; width:29rem;height: 3rem;font-size: 1.3rem;">
-<option value = "gname" style = "background-color: white;">物品名称</option>
-<option value = "variety" style = "background-color: white;">物品种类</option>
-<option value = "belong" style = "background-color: white;">归属部门</option>
-</select>
-<input type="text" class="form-control" placeholder="查询条件" style = "text-align: center; width: 60rem; height: 3rem;"/>
+<?php 
+echo "<select class='form-control' name = 'condition' style = 'background-color: whiteSmoke;border-style: outset; padding-left: 11rem; width:29rem;height: 3rem;font-size: 1.3rem;'>";
+
+if (!isset($_POST['condition']) || $_POST['condition'] == 'gname')
+    echo"<option value = 'gname' style = 'background-color: white;'>物品名称</option>";
+else 
+    echo"<option value = 'gname' selected = 'selected' style = 'background-color: white;'>物品名称</option>";
+if (isset($_POST['condition']) && $_POST['condition'] == 'variety')
+    echo"<option value = 'variety' selected = 'selected' style = 'background-color: white;'>物品种类</option>";
+else 
+    echo"<option value = 'variety' style = 'background-color: white;'>物品种类</option>";
+if (isset($_POST['condition']) && $_POST['condition'] == 'belong')
+    echo "<option value = 'belong' selected = 'selected' style = 'background-color: white;'>归属部门</option>";
+else
+    echo"<option value = 'belong' style = 'background-color: white;'>归属部门</option>";
+echo"</select>";
+
+if(isset($_POST['sValue']))
+    echo "<input type='text' class='form-control' name = 'sValue' value = '".$_POST['sValue']."'' placeholder='查询条件' style = 'text-align: center; width: 60rem; height: 3rem;'>";
+else 
+    echo "<input type='text' class='form-control' name = 'sValue' value = '' placeholder='查询条件' style = 'text-align: center; width: 60rem; height: 3rem;'>";
+?>
 </div>
 
-
-<button type="submit" onclick="select1()" class="btn btn-default search" name = "search_goods" >查询物资</button>
-<button type="submit" onclick="select2()" class="btn btn-default search" name = "edit_goods">编辑物资</button>
-<button type="submit" onclick="select3()" class="btn btn-default search" name = "user_borrow">我的借用</button>
+<button type="submit" class="btn btn-default search" name = "search_goods" >查询物资</button>
+<button type="submit" class="btn btn-default search" name = "edit_goods">编辑物资</button>
+<button type="submit" class="btn btn-default search" name = "user_borrow">我的借用</button>
 </div>
-
-<script>
-function select1(){
-    var page = 1;
-    change_page();
-}
-function select2(){
-    var page = 2;
-    change_page();
-}
-function select3(){
-    var page = 3;
-    change_page();
-}
-</script>
+</form>
 
 <?php
     define("ACCESS_CODE_TIMEOUT", 300);
@@ -100,17 +102,16 @@ function select3(){
     } catch(PDOException $e) {
         echo $e->getMessage();
     }
-    $page = 1;
     change_page();
 
 function change_page(){
     global $page, $dbh;
 
-    if($page == 3){
+    if(isset($_POST['user_borrow'])){
         /*$sql = $dbh->prepare("select * from borrow  where uid = ?");//从数据库中查询数据
         $sql->execute($_COOKIE['user']);*/
-        $sql = $dbh->prepare("select * from borrow natural join goods where uid = '3150103737' order by apply_date");
-        $sql->execute();
+        $sql = $dbh->prepare("select * from borrow natural join goods where uid = ? and ".$_POST['condition']." like '%".$_POST['sValue']."%' order by apply_date;");
+        $sql->execute(array($_COOKIE['user']));
         $row = $sql->fetchAll(PDO::FETCH_ASSOC);
 
         echo "<div align = 'center'><table style = 'width: 100%; line-height: 4rem; padding: 1rem; margin:2rem 0rem'>
@@ -139,11 +140,11 @@ function change_page(){
             echo "<td color = grey>" . $key['Preturn'] . "</td>";
         else
             echo "<td>" . $key['return_date'] . "</td>";
-        if(is_null($key['borrow_date']))echo "<td><a href = 'whatever'><img class = icon src = 'icon/borrow.png'>确认借用</a>
+        if(is_null($key['borrow_date']))echo "<td><a href = 'whatever'><img class = icon src = 'icon/borrow.png'>待借出</a>
         </td>";
-        else if (is_null($key['return_date']))echo"<td><a href = 'whatever'><img class = icon src = 'icon/return.png'>确认归还</a>
+        else if (is_null($key['return_date']))echo"<td><a href = 'whatever'><img class = icon src = 'icon/return.png'>待归还</a>
         </td>";
-        else echo"<td><a href = 'whatever' color = grey><img class = icon src = 'icon/return.png'>借用完毕</a>
+        else echo"<td><a href = 'whatever' color = grey><img class = icon src = 'icon/return.png'>已归还</a>
         </td>";
         echo"<td><a href = 'whatever'><img class = icon src = 'icon/edit.png'>编辑</a>
         </td>";
@@ -153,7 +154,10 @@ function change_page(){
         echo "</table></div>";
     }
     else {
-        $sql = $dbh->prepare("SELECT * FROM goods");
+        if(isset($_POST['condition']) && isset($_POST['sValue']))
+            $sql = $dbh->prepare("SELECT * FROM goods where ".$_POST['condition']." like '%".$_POST['sValue']."%'");
+        else 
+            $sql = $dbh->prepare("SELECT * FROM goods");
         $sql->execute();
         $row = $sql->fetchAll(PDO::FETCH_ASSOC);
 
